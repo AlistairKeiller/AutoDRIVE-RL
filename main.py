a6e8e645b5697a94c673ee58d40139ebda4d4526
Alistair Keiller <alistair@keiller.net>
2025-07-10 22:32:47 -0700

    get ppo working!!!

--- main.py
+++
@@ -1,9 +0,0 @@
-from mlagents_envs.environment import UnityEnvironment  # type: ignore
-from gym_unity.envs import UnityToGymWrapper  # type: ignore
-from stable_baselines3 import PPO
-
-if __name__ == "__main__":
-    print("Starting PPO training script...")
-    unity_env = UnityEnvironment()
-    env = UnityToGymWrapper(unity_env)  # type: ignore
-    model = PPO("MlpPolicy", env, verbose=1).learn(10_000)
